d522ef6f324d9705b03d970784e43452417debbf
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-03-23 20:32:55 +0100

    First Commit and project setup

---
+++ src/main/java/sketch01/Sketch01.java
@@ -0,0 +1,10 @@
+package sketch01;
+
+public class Sketch01 {
+    static void main() {
+        IO.println("Hello and welcome!");
+        for (int i = 1; i <= 5; i++) {
+            IO.println("i = " + i);
+        }
+    }
+}
