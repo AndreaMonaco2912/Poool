91053f5c04b8b98700741e83598f8c8a010210e9
Andrea Monaco <andrea.monaco4@studio.unibo.it>
2026-04-16 01:09:29 +0200

    Changed ball split in round robin based split

--- src/main/java/sketch/impl/model/BallManager.java
+++ src/main/java/sketch/impl/model/BallManager.java
@@ -8,17 +8,13 @@ import java.util.List;
 public record BallManager(Ball playerBall, Ball cpuBall, List<Ball> balls, List<Ball> holes) {
 
     public List<List<Ball>> splitSimpleBalls(int n) {
-        List<Ball> list = new ArrayList<>(balls);
+        List<Ball> allBalls = new ArrayList<>(balls);
         List<List<Ball>> result = new ArrayList<>();
-        int size = list.size();
-        int baseSize = size / n;
-        int extra = size % n;
-
-        int index = 0;
         for (int i = 0; i < n; i++) {
-            int count = baseSize + (i < extra ? 1 : 0);
-            result.add(new ArrayList<>(list.subList(index, index + count)));
-            index += count;
+            result.add(new ArrayList<>());
+        }
+        for (int i = 0; i < allBalls.size(); i++) {
+            result.get(i % n).add(allBalls.get(i));
         }
         return result;
     }
